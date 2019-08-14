bb67879def0772d31cb13a1547b3fd162ae3c751
Samvel <41449047+sttie@users.noreply.github.com>
2019-08-15 01:42:28 +0300

    Update brainfuck.c

--- brainfuck.c
+++ brainfuck.c
@@ -27,19 +27,24 @@ void parse(char *str) {
         current == '<' ? indexn-- : indexn;
         current == '+' ? program[indexn]++ : program[indexn];
         current == '-' ? program[indexn]-- : program[indexn];
-
-        if (current == '.') {
-            printf("%c", program[indexn]);
-        }
-
+        
+        if      (current == '>') indexn++;
+        else if (current == '<') indexn--;
+        else if (current == '+') program[indexn]++;
+        else if (current == '-') program[indexn]--;
+        else if (current == '.') printf("%c", program[indexn]);
+        
         else if (current == '[') {
             push(i++, &st);
+            
             int end;
+            
             while (program[indexn]) {
                 parse(str);
                 end = pop(&st);
                 i = pop(&st);
             }
+            
             i = end;
         }
 
